81ea7f63dcd4da2270f2fae96f15421bc9ff5c9c
“koichi-utebi <k.utebi.1225@gmail.com>
2022-07-21 00:49:00 +0000

    finish 1

---
+++ main.java
@@ -0,0 +1,41 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+
+    int number1 = 3;い
+    int number2 = 7;
+    System.out.println(number1 * number2);
+
+    String text = "プログラミングを勉強しよう";
+    System.out.println("Progateで"+text);
+    
+    number1 *= 7;
+    System.out.println(number);
+    
+    number1 ++;
+    System.out.println(number);
+    
+    int length = 6;
+    int height = 8;
+
+    int rectangleArea = length * height;
+    System.out.println(rectangleArea);
+
+    int triangleArea = length * height / 2;
+    System.out.println(triangleArea);
+    
+    
+    double number3 = 8.5;
+    double number4 = 3.4;
+    System.out.println(number3 + number4);
+    System.out.println(number3 - number4);
+    
+    
+    int number5 = 7;
+    int number6 = 2;
+    System.out.println(number5 / number6);
+    
+    // number1をdouble型にキャストし、number2で割った値を出力してください
+    System.out.println((double)number5 / number6);
+  }
+}
